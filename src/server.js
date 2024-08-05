7ab6ae28b60c259d11d20db6259f58595218b8f4
kootaeho <0307rnxogh@daum.net>
2024-08-05 22:02:36 +0900

    addHttpServer/wsServer

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -6,6 +8,12 @@ app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req,res) => res.render("home"));
+
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
-app.listen(3000, handleListen );
+const server = http.createServer(app);  //express 서버랑 http 합치기
+
+const wss = new WebSocket.Server({server}); //  http서버위에 웹소켓 서버 합치기
+
+server.listen(3000,handleListen);
+
